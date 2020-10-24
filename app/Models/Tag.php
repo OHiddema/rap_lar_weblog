3280d2639988e5147588cf20972235dac593355c
Oege Hiddema <oegehiddema@gmail.com>
2020-10-24 11:46:07 +0200

    Laracast ep. 31, fixed errors

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -11,6 +11,6 @@ class Tag extends Model
 
     public function articles()
     {
-        return $this->belongstoToMany(Article::class);
+        return $this->belongsToMany(Article::class);
     }
 }
