21a6ae36ce71ef39f91fb69d91fb25c524e1eea5
Oege Hiddema <oegehiddema@gmail.com>
2020-11-04 14:59:04 +0100

    dashboard, version 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class DashboardController extends Controller
 {
@@ -11,6 +12,25 @@ class DashboardController extends Controller
         $users = \App\Models\User::all();
         $articles = \App\Models\Article::all();
         $comments = \App\Models\Comment::all();
-        return view('dashboard.index',['users' => $users,'articles'=>$articles,'comments'=>$comments]);
+        $roles = $users->pluck('role')->countBy();
+
+        $articles_per_user = DB::table('articles')
+            ->join('users', 'users.id', '=', 'articles.user_id')
+            ->select('users.name','articles.id')
+            ->pluck('users.name')->countBy();            
+
+        $comments_per_user = DB::table('comments')
+            ->join('users', 'users.id', '=', 'comments.user_id')
+            ->select('users.name','comments.id')
+            ->pluck('users.name')->countBy();            
+
+        return view('dashboard.index',
+            ['users' => $users,
+            'articles' => $articles,
+            'comments' => $comments,
+            'roles' => $roles,
+            'articles_per_user' => $articles_per_user,
+            'comments_per_user' => $comments_per_user
+            ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,9 +3,35 @@
 @section('content')
    @can('dashboard', Auth::user())
       <h1>Dashboard</h1>
-      <p>Users: {{$users->count()}}</p>
-      <p>Articles: {{$articles->count()}}</p>
-      <p>Comments: {{$comments->count()}}</p>
-   @endcan
+      <div class="container rounded border border-dark" style="background-color: rgb(180, 255, 199)">
+         <p>Total number of users: {{$users->count()}}</p>
+         Roles:
+         <div class="container rounded border border-dark mb-1" style="background-color: rgb(180, 234, 255)">
+            @foreach ($roles as $key => $value)
+               <div>{{$key}}: {{$value}} </div>
+            @endforeach            
+         </div>
+      </div>
+
+      <div class="container rounded border border-dark mt-2" style="background-color: rgb(180, 255, 199)">
+         <p>Total number of articles: {{$articles->count()}}</p>
+         Written by:
+         <div class="container rounded border border-dark mb-1" style="background-color: rgb(180, 234, 255)">
+            @foreach ($articles_per_user as $key => $value)
+               <div>{{$key}}: {{$value}} </div>
+            @endforeach            
+         </div>
+      </div>
+
+      <div class="container rounded border border-dark mt-2" style="background-color: rgb(180, 255, 199)">
+         <p>Total number of comments: {{$comments->count()}}</p>
+         Written by:
+         <div class="container rounded border border-dark mb-1" style="background-color: rgb(180, 234, 255)">
+            @foreach ($comments_per_user as $key => $value)
+               <div>{{$key}}: {{$value}} </div>
+            @endforeach            
+         </div>
+      </div>
+      @endcan
 
 @endsection
