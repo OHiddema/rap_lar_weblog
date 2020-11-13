3a13eeeb85ad952756e049485409d474e4baad23
Oege Hiddema <oegehiddema@gmail.com>
2020-11-13 18:58:39 +0100

    remove admin check in view (already done in controller)

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -1,37 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-   @can('admin', Auth::user())
-      <h1>Users</h1>
-      <table class="table table-sm">
-         <thead class="thead-light">
+   <h1>Users</h1>
+   <table class="table table-sm">
+      <thead class="thead-light">
+         <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th></th>
+         </tr>
+      </thead>
+      <tbody>
+         @foreach ($users as $user)
             <tr>
-               <th>Name</th>
-               <th>Email</th>
-               <th>Role</th>
-               <th></th>
+               <td>{{$user->name}}</td>
+               <td>{{$user->email}}</td>
+               <td>{{$user->role}}</td>
+               <td>
+                  <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}">Show</a>
+                  <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}/edit">Edit</a>
+                  <div class="d-inline-block">
+                     <form action="/dashboard/users/{{$user->id}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
+                     </form>
+                  </div>
+               </td>            
             </tr>
-         </thead>
-         <tbody>
-            @foreach ($users as $user)
-               <tr>
-                  <td>{{$user->name}}</td>
-                  <td>{{$user->email}}</td>
-                  <td>{{$user->role}}</td>
-                  <td>
-                     <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}">Show</a>
-                     <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}/edit">Edit</a>
-                     <div class="d-inline-block">
-                        <form action="/dashboard/users/{{$user->id}}" method="POST">
-                           @csrf
-                           @method('DELETE')
-                           <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
-                        </form>
-                     </div>
-                  </td>            
-               </tr>
-            @endforeach
-         </tbody>
-      </table>
-   @endcan
+         @endforeach
+      </tbody>
+   </table>
 @endsection
