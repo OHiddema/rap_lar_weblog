05af36b81524e0bfa95b9f313254fb818313b103
Oege Hiddema <oegehiddema@gmail.com>
2020-11-13 18:52:52 +0100

    styling

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -3,50 +3,35 @@
 @section('content')
    @can('admin', Auth::user())
       <h1>Users</h1>
-      <div class="container">
-         <div class="row border">
-            <div class="col-3 border font-weight-bold">
-               Name
-            </div>
-            <div class="col-3 border border font-weight-bold">
-               Email
-            </div>
-            <div class="col-1 border border font-weight-bold">
-               Role
-            </div>
-            {{-- <div class="col-3 border border font-weight-bold">
-               Created_at
-            </div> --}}
-            <div class="col-5 border border font-weight-bold">
-            </div>
-         </div>
-      @foreach ($users as $user)
-            <div class="row border">
-               <div class="col-3 border">
-                  {{$user->name}}
-               </div>
-               <div class="col-3 border">
-                  {{$user->email}}
-               </div>
-               <div class="col-1 border">
-                  {{$user->role}}
-               </div>
-               {{-- <div class="col-3 border">
-                  {{date_format($user->created_at,"d/m/Y G:i:s")}}
-               </div> --}}
-               <div class="col-5 border">
-                  <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}">Show</a>
-                  <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}/edit">Edit</a>
-                  <div class="d-inline-block">
-                     <form action="/dashboard/users/{{$user->id}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
-                     </form>
-                  </div>
-               </div>
-            </div>
-         @endforeach
-      </div>
+      <table class="table table-sm">
+         <thead class="thead-light">
+            <tr>
+               <th>Name</th>
+               <th>Email</th>
+               <th>Role</th>
+               <th></th>
+            </tr>
+         </thead>
+         <tbody>
+            @foreach ($users as $user)
+               <tr>
+                  <td>{{$user->name}}</td>
+                  <td>{{$user->email}}</td>
+                  <td>{{$user->role}}</td>
+                  <td>
+                     <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}">Show</a>
+                     <a class="btn btn-primary btn-sm" href="/dashboard/users/{{$user->id}}/edit">Edit</a>
+                     <div class="d-inline-block">
+                        <form action="/dashboard/users/{{$user->id}}" method="POST">
+                           @csrf
+                           @method('DELETE')
+                           <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
+                        </form>
+                     </div>
+                  </td>            
+               </tr>
+            @endforeach
+         </tbody>
+      </table>
    @endcan
 @endsection
--- resources/views/dashboard/users/show.blade.php
+++ resources/views/dashboard/users/show.blade.php
@@ -3,13 +3,34 @@
 @section('content')
 <a class="btn btn-primary" href="/dashboard/users">All users</a><br>
 
-<div class="container rounded border border-dark mt-2 pt-2 pb-2" style="background-color: rgb(180, 234, 255)">
-   <div><b>Name: </b>{{$user->name}}</div>
-   <div><b>Email: </b>{{$user->email}}</div>
-   <div><b>Role: </b>{{$user->role}}</div>
-   <div><b>Created at: </b>{{date_format($user->created_at,"d/m/Y G:i")}}</div>
-   <div><b>Updated at: </b>{{date_format($user->updated_at,"d/m/Y G:i")}}</div>
-</div>
+<table class="table table-sm mt-4">
+   <tbody>
+      <tr>
+         <td>Name:</td>
+         <td>{{$user->name}}</td>
+      </tr>
+      <tr>
+         <td>Email:</td>
+         <td>{{$user->email}}</td>
+      </tr>
+      <tr>
+         <td>Profile image:</td>
+         <td><img class="profile-image" src="{{ $user->image }}"></td>
+      </tr>
+      <tr>
+         <td>Role:</td>
+         <td>{{$user->role}}</td>
+      </tr>
+      <tr>
+         <td>Created at:</td>
+         <td>{{date_format($user->created_at,"d/m/Y G:i")}}</td>
+      </tr>
+      <tr>
+         <td>Updated at:</td>
+         <td>{{date_format($user->updated_at,"d/m/Y G:i")}}</td>
+      </tr>
+   </tbody>
+</table>
 
 <div class="container rounded border border-dark mt-2 pt-2 pb-2" style="background-color: rgb(180, 255, 199)">
    <h5 class="font-weight-bold">This user has:</h5>
