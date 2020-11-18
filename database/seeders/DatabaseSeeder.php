ff13eb77c698fb4ad64ba0e3887c27d873f35a01
Oege Hiddema <oegehiddema@gmail.com>
2020-11-18 09:59:55 +0100

    create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        // Users and tags already created manually
+
+        // Create 100 articles and 3 comments for each article 
+        \App\Models\Article::factory()
+            ->times(100)
+            ->hasComments(3)
+            ->create();
+
+        // Give each article one random tag and one random like
+        $articles = \App\Models\Article::all();
+        foreach ($articles as $article) {
+            DB::table('article_tag')->insert([
+                'article_id' => $article->id,
+                'tag_id' => \App\Models\Tag::inRandomOrder()->first()->id
+            ]);
+            DB::table('likes')->insert([
+                'article_id' => $article->id,
+                'user_id' => \App\Models\User::inRandomOrder()->first()->id
+            ]);
+        }
     }
 }
