d071ebe9df6d6dfcc3bb7661283e45f3a176aacf
Oege Hiddema <oegehiddema@gmail.com>
2020-11-19 10:50:09 +0100

    use Mailer class

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -13,10 +13,13 @@ class NotificationController extends Controller
 
     public function store() {
         request()->validate(['email'=>'required|email']);
-        Mail::raw('plain text message', function ($message) {
-            $message->to(request('email'));
-            $message->subject('Subject');
-        });
+
+        // Mail::raw('plain text message', function ($message) {
+        //     $message->to(request('email'));
+        //     $message->subject('Subject');
+        // });
+
+        Mail::to(request('email'))->send(new \App\Mail\ContactMe(auth()->user()->name));
 
         return redirect('/contact')
         ->with('message','Email sent!');
